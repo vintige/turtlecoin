03705cbff8886de934d17f793deb3e2c00c225dc
XARIA <37904871+xaria-coin@users.noreply.github.com>
2018-07-31 18:29:44 +0100

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -136,7 +136,14 @@ static_assert(UPGRADE_VOTING_WINDOW > 1, "Bad UPGRADE_VOTING_WINDOW");
 /* Block heights we are going to have hard forks at */
 const uint64_t FORK_HEIGHTS[] =
 {
-    262800
+    187000,
+    350000,
+    440000,
+    620000,
+    800000,
+    1000000,
+    1200000,
+    1400000
 };
 
 const uint64_t FORK_HEIGHTS_SIZE = sizeof(FORK_HEIGHTS) / sizeof(*FORK_HEIGHTS);
