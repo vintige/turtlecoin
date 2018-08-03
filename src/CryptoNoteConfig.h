21fece49f48be7c7cb70547123bde2c844bac2c7
XARIA <37904871+xaria-coin@users.noreply.github.com>
2018-08-03 20:04:56 +0100

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -80,13 +80,13 @@ const uint64_t MINIMUM_FEE                                   = UINT64_C(0);
 
 const uint64_t MINIMUM_MIXIN_V1                              = 0;
 const uint64_t MAXIMUM_MIXIN_V1                              = 3;
-const uint64_t MINIMUM_MIXIN_V2                              = 2;
+const uint64_t MINIMUM_MIXIN_V2                              = 1;
 const uint64_t MAXIMUM_MIXIN_V2                              = 7;
 
 const uint32_t MIXIN_LIMITS_V1_HEIGHT                        = 0;
 const uint32_t MIXIN_LIMITS_V2_HEIGHT                        = 1;
 
-const uint64_t DEFAULT_MIXIN                                 = MINIMUM_MIXIN_V2;
+const uint64_t DEFAULT_MIXIN                                 = 5;
 
 const uint64_t DEFAULT_DUST_THRESHOLD                        = UINT64_C(10);
 const uint64_t DEFAULT_DUST_THRESHOLD_V2                     = UINT64_C(10);
