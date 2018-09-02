659c8158acb026904373cce59bf05813e8df859c
XARIA <37904871+xaria-coin@users.noreply.github.com>
2018-09-02 17:05:36 +0100

    Update CryptoNoteCheckpoints.h

--- src/CryptoNoteCheckpoints.h
+++ src/CryptoNoteCheckpoints.h
@@ -28,7 +28,5 @@ struct CheckpointData {
 
 const std::initializer_list<CheckpointData> CHECKPOINTS = {  
 {       4000, "20a7ff7d113977b8ebacbed02270cfa3b6fff3f6cc84e65b4daf069d3c9e3079"}
-
-
 };
 }
